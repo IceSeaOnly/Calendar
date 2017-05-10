01ecb6dd35ec2739e222401580cf5c4dda118022
IceSeaOnly <1041414957@qq.com>
2017-05-10 12:03:47 +0800

    init

---
+++ src/main/java/site/binghai/CarlendarApplication.java
@@ -0,0 +1,12 @@
+package site.binghai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlendarApplication.class, args);
+	}
+}
